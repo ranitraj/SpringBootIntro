38943225daf9c4efc545c601d1fe0da95d4da3a0
ranitrajganguly@gmail.com <ranitrajganguly@gmail.com>
2023-01-31 23:34:02 -0700

    Adding Student model class

---
+++ src/main/java/com/example/springIntro/student/Student.java
@@ -0,0 +1,92 @@
+package com.example.springIntro.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String email;
+
+    // Constructors
+    public Student() {}
+
+    public Student(
+            long id,
+            String name,
+            LocalDate dob,
+            Integer age,
+            String email
+    ) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Student(
+            String name,
+            LocalDate dob,
+            Integer age,
+            String email
+    ) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
